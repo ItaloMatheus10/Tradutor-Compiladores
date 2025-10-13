d2f74f585d36197726a2957026a51e9a3e692ea2
ItaloMatheus10 <130625413+ItaloMatheus10@users.noreply.github.com>
2025-10-13 23:21:49 +0000

    Teste de erro na main

--- tradutor-simples/src/main/java/com/compiladores/Main.java
+++ tradutor-simples/src/main/java/com/compiladores/Main.java
@@ -2,7 +2,7 @@ package com.compiladores;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String input = "8+5-7+9";
+        String input = "8*9";
         Parser p = new Parser (input.getBytes());
         p.parse();
 
