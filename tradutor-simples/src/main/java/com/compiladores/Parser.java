a92f497017a067bdd3aa21356a1dac3145f1a742
ItaloMatheus10 <130625413+ItaloMatheus10@users.noreply.github.com>
2025-10-13 23:15:20 +0000

    Adicionando métodos auxiliares

--- tradutor-simples/src/main/java/com/compiladores/Parser.java
+++ tradutor-simples/src/main/java/com/compiladores/Parser.java
@@ -12,4 +12,18 @@ public class Parser {
         // aqui ainda ira o código
     }
 
+    private char peek () {
+        if (current < input.length)
+           return (char)input[current];
+       return '\0';
+    }
+
+    private void match (char c) {
+        if (c == peek()) {
+            current++;
+        } else {
+            throw new Error("syntax error");
+        }
+  }
+
 }
