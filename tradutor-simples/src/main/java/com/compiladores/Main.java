59ee19dee8193dd039f3084f72ba2026f4308078
ItaloMatheus10 <130625413+ItaloMatheus10@users.noreply.github.com>
2025-10-13 23:36:47 +0000

    Execução da main com os métodos atualizados

--- tradutor-simples/src/main/java/com/compiladores/Main.java
+++ tradutor-simples/src/main/java/com/compiladores/Main.java
@@ -1,14 +1,10 @@
 package com.compiladores;
 
 public class Main {
-    public static void main(String[] args)  {
-        String input = "4-8+6";
-        Scanner scan = new Scanner (input.getBytes());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
+    public static void main(String[] args) {
+        String input = "8+5-7+9";
+        Parser p = new Parser (input.getBytes());
+        p.parse();
 
     }
 }
