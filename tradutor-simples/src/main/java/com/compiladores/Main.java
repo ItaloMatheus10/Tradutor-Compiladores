2c75d1fced3844fe55844eccde216d184a397877
ItaloMatheus10 <130625413+ItaloMatheus10@users.noreply.github.com>
2025-10-14 00:14:54 +0000

    Teste da main com os identificadores

--- tradutor-simples/src/main/java/com/compiladores/Main.java
+++ tradutor-simples/src/main/java/com/compiladores/Main.java
@@ -1,10 +1,15 @@
 package com.compiladores;
 
+import com.compiladores.Scanner.Token;
+import com.compiladores.Scanner.TokenType;
+
 public class Main {
    public static void main(String[] args) {
         String input = "45  + preco - 876";
-        Parser p = new Parser (input.getBytes());
-        p.parse();
+        Scanner scan = new Scanner (input.getBytes());
+        for (Token tk = scan.nextToken(); tk.type != TokenType.EOF; tk = scan.nextToken()) {
+        System.out.println(tk);
+    }
     
     }
 }
